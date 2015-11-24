3560d149338125b6857a0c746aee8b37e4fe40e6
Maranux <shea@splatformers.com>
2015-11-24 10:57:01 -0500

    Update the Connection and Main classes.

--- CSCI-FTP/src/com/csci/Connection.java
+++ CSCI-FTP/src/com/csci/Connection.java
@@ -1,4 +1,8 @@
 package com.csci;
+import jdk.internal.util.xml.impl.Input;
+
+import java.io.*;
+import java.net.*;
 
 /**
  * Created by shea on 11/5/2015.
@@ -8,6 +12,63 @@ package com.csci;
  * Receive: The encoded file from the file server.
  * Receive: The encoded file from Upload.
  */
-public class Connection {
+class Connection {
 
+    // The actual connection
+    private URLConnection urlConnection;
+
+    private String hostURL;
+    private String userLogin;
+    private String userPassword;
+
+    public boolean uploadConnection(InputStream inputStream) throws IOException {
+        try {
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+            OutputStream outputStream = urlConnection.getOutputStream();
+            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream);
+            byte[] byteBuffer = new byte[4096];
+            int counter;
+
+            while ((counter = bufferedInputStream.read(byteBuffer)) > 0) {
+
+                bufferedOutputStream.write(byteBuffer, 0, counter);
+
+            }
+
+            bufferedOutputStream.close();
+
+            return true;
+
+        } catch (Exception ex){
+
+            ex.printStackTrace();
+            return false;
+
+        }
+    }
+
+    public void setHostURL(String URL){
+        this.hostURL = URL;
+    }
+
+    public void setUserLogin(String userLogin) {
+        this.userLogin = userLogin;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public boolean connectToServer() {
+
+        try {
+            URL host = new URL("ftp://" + userLogin + ":" + userPassword + "@" + hostURL);
+            urlConnection = host.openConnection();
+            return true;
+        } catch(Exception ex){
+            ex.printStackTrace();
+            return false;
+        }
+    }
 }
+
--- CSCI-FTP/src/com/csci/Main.java
+++ CSCI-FTP/src/com/csci/Main.java
@@ -1,8 +1,22 @@
 package com.csci;
 
+import sun.net.util.IPAddressUtil;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+
 public class Main {
+    // On return from the JFrame, call the other functions.
+    // May need connection security info.
+    public String ipAddress;
+    public int Port;
+    public String UserName;
+    public String Password;
+    public SocketAddress socketAddress;
 
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
