e385e335d97949536994fa9060f1e6ce7619ef44
Maranux <shea@splatformers.com>
2015-11-27 07:40:25 -0500

    Updated Main not to use sockets.

--- CSCI-FTP/src/com/csci/Main.java
+++ CSCI-FTP/src/com/csci/Main.java
@@ -1,12 +1,5 @@
 package com.csci;
 
-import sun.net.util.IPAddressUtil;
-
-import java.net.Inet4Address;
-import java.net.InetAddress;
-import java.net.Socket;
-import java.net.SocketAddress;
-
 public class Main {
     // On return from the JFrame, call the other functions.
     // May need connection security info.
@@ -14,7 +7,6 @@ public class Main {
     public int Port;
     public String UserName;
     public String Password;
-    public SocketAddress socketAddress;
 
     public static void main(String[] args) {
 
